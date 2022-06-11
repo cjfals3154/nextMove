a70792fd99ed295d19a6b79ad84d1b9baa137816
Shin <cjfals3154@naver.com>
2022-06-11 11:57:22 +0900

    css

--- pages/_document.js
+++ pages/_document.js
@@ -37,6 +37,7 @@ class MyDocument extends Document {
         <Head />
         <body>
           <Main />
+          <script src="https://polyfill.io/v3/polyfill.min.js?features=Intl%2Cdefault%2Ces2015%2Ces2016%2Ces2018%2Ces2017%2Ces2019%2Ces2020%2Ces2021" />
           <NextScript />
         </body>
       </Html>
